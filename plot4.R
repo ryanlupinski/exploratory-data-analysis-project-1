770944cc8a8da4b1aa28e0c0c3b47a03f5ca3974
Ryan Lupinski <ryanl1349@gmail.com>
2015-07-11 15:36:50 -0400

    fixed plot4.R for missing code

--- plot4.R
+++ plot4.R
@@ -33,10 +33,17 @@ voltage <- as.numeric(data_req$Voltage)
 subMetering1 <- as.numeric(data_req$Sub_metering_1)
 subMetering2 <- as.numeric(data_req$Sub_metering_2)
 subMetering3 <- as.numeric(data_req$Sub_metering_3)
-# Global Active Power 
+# Global Active Power  
 globalReactivePower <- as.numeric(data_req$Global_reactive_power)
 
 # plot all 4 graphs in 1 window
 png("plot4.png", width=480, height=480,)
-
-
+par(mfrow=c(2,2))
+plot(datetime, globalActivePower, type ="l", ylab ="Global Active Power (kilowawtts)")
+plot(datetime, voltage, type="l", ylab="Voltage")
+plot(datetime,subMetering1, ylim =range(c(subMetering1,subMetering2)), type ="l", ylab="Energy Submetering", xlab="")
+lines(datetime,subMetering2, ylim =range(c(subMetering1,subMetering2)), type ="l", col="red")
+lines(datetime,subMetering3, ylim =range(c(subMetering1,subMetering2)), typ = "l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2, col=c("black", "red", "blue"))
+plot(datetime, globalReactivePower, type="l", ylab="Global_Reactive_Power")
+dev.off()
